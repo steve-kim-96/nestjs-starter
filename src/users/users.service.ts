4ed442138a0616e398c6d03116b7561c2c3460e9
steve-kim-96 <soungku@hotmail.com>
2021-11-25 20:08:46 +1300

    implement CRUD for user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const user = await this.userRepository.create(createUserDto);
+      return this.userRepository.save(user);
+    } catch (error) {
+      console.error("Woops. Couldn't create user");
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    try {
+      const users = await this.userRepository.find();
+      return users;
+    } catch (error) {
+      console.error("Woops. Couldn't find the users");
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<User> {
+    try {
+      const user = this.userRepository.findOne(id);
+      return user;
+    } catch (error) {
+      console.error("Woops. Couldn't find that user");
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<UpdateResult> {
+    try {
+      const updatedUser = this.userRepository.update(id, updateUserDto);
+      return updatedUser
+    } catch (error) {
+      console.error("Woops, couldn't update that user");
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string): Promise<DeleteResult> {
+    try {
+      const deletedUser = this.userRepository.delete(id);
+      return deletedUser;
+    } catch (error) {
+      console.error("Woops. Couldn't delete that user");
+    }
   }
 }
