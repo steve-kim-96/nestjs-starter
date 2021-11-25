774c60a9111c5f284a5434b5b484d5ef36305adb
steve-kim-96 <soungku@hotmail.com>
2021-11-25 20:24:09 +1300

    create user interface and entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,26 +1,26 @@
-import { Post } from "src/posts/entities/post.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Post } from 'src/posts/entities/post.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({
-        name: "username"
-    })
-    username: string;
+  @Column({
+    name: 'username',
+  })
+  username: string;
 
-    @Column({
-        name: "first_name"
-    })
-    firstName: string;
+  @Column({
+    name: 'first_name',
+  })
+  firstName: string;
 
-    @Column({
-        name: "last_name"
-    })
-    lastName: string;
+  @Column({
+    name: 'last_name',
+  })
+  lastName: string;
 
-    @OneToMany(() => Post, post => post.user)
-    posts: Post[];
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
 }
