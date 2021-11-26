4a3179c6a99b22572a903c6070537abeef412dc2
steve-kim-96 <soungku@hotmail.com>
2021-11-27 08:47:38 +1300

    set up for auth in module. subsequent packages added

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { Service } from './auth.service';
 
-@Module({})
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {expiresIn: '100s'}
+      }),
+    }),
+  ],
+  providers: [Service],
+})
 export class AuthModule {}
