e87e154b0e3a9a6f0ad056cb056f59d4f0049ee2
steve-kim-96 <soungku@hotmail.com>
2021-12-03 08:15:09 +1300

    implement login in controller

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { IUser } from './entities/user.interface';
+import { catchError, map, Observable, of } from 'rxjs';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(@Body() createUserDto: CreateUserDto): Observable<IUser | object> {
+    return this.usersService.create(createUserDto).pipe(
+      map((user: IUser) => user),
+      catchError((err) => of({ error: err.message })),
+    );
+  }
+
+  @Post('login')
+  login(@Body() user: User): Observable<object> {
+    return this.usersService.login(user).pipe(
+      map((jwt: string) => {
+        return { access_token: jwt };
+      }),
+    );
   }
 
   @Get()
